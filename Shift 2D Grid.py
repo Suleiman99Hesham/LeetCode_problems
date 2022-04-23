5f37d4cf70ceac6975f3e981c8e563b0f24cb2a4
Suleiman99Hesham <suleimanhesham99@gmail.com>
2022-04-24 01:21:50 +0200

    hot fix

--- Shift 2D Grid.py
+++ Shift 2D Grid.py
@@ -5,24 +5,23 @@ class Solution:
     def shiftGrid(self, grid: List[List[int]], k: int) -> List[List[int]]:
         m = len(grid)
         n = len(grid[0])
-        print('K:',k)
-        print('m*n:',(m*n))
-        print('abs:',abs((m*n)-k))
-        print('abs&(m*n):',abs((m*n)-k)%(m*n))
-
-        cut_from_index = abs((m*n)-k)%(m*n)
-        if cut_from_index == 0 :
+        if k >= (m*n):
+            actual_shifts = abs((m*n)-k)%(m*n)
+        else:
+            actual_shifts = k
+        if actual_shifts == 0 :
             return grid
-        actual_shifts = (m*n)-cut_from_index
+        cut_from_index = (m*n)-actual_shifts
         shifted_one_dimensional_list = []
         for i in range(cut_from_index, m*n):
-            shifted_one_dimensional_list.append(grid[i//m][i%n])
+            x,y = i//m, i%n
+            shifted_one_dimensional_list.append(grid[x][y])
         for i in range(cut_from_index):
             shifted_one_dimensional_list.append(grid[i//m][i%n])
         result = []
         row=[]
         for i in range(m*n):
-            if len(row) == m:
+            if len(row) == n:
                 result.append(row)
                 row=[]
             row.append(shifted_one_dimensional_list[i])
@@ -30,8 +29,14 @@ class Solution:
         return result
 
 
-grid = [[3,8,1,9],[19,7,2,5],[4,6,11,10],[12,0,21,13]]
+grid = [[1,2,3],[4,5,6],[7,8,9],[10,11,12],[13,14,15],[16,17,18]]
+
 
 obj = Solution()
-print(obj.shiftGrid(grid,17))
-print(abs(16-17)%16)
+print(obj.shiftGrid(grid,2))
+print('1//3:', 1//3)
+print('3//1:', 3//1)
+print('3//3:', 3//3)
+print('1%3:', 1%3)
+print('3%1:', 3%1)
+print('3%3:', 3%3)
