9dd14ed7cc9fbe8613ce64d65352b5fac3244588
Suleiman Hesham <45403646+Suleiman99Hesham@users.noreply.github.com>
2022-09-03 02:05:34 +0200

    Time: 84 ms (43.70%), Space: 17.3 MB (8.85%) - LeetHub

--- 637-average-of-levels-in-binary-tree/637-average-of-levels-in-binary-tree.py
+++ 637-average-of-levels-in-binary-tree/637-average-of-levels-in-binary-tree.py
@@ -6,25 +6,28 @@ class TreeNode:
 
 class Solution:
     def averageOfLevels(self, root: TreeNode):
-        sums = []
-        counts = []
-        level = 0
+        counts = {}
         def collect_values(node, level):
-            if level >= len(sums):
-                sums.append(0)
-            if level >= len(counts):
-                counts.append(0)
-            sums[level] += node.val
-            counts[level]  +=1 
+            if level in counts:
+                counts[level][0] += node.val
+                counts[level][1] += 1
+            else:
+                counts[level] = [node.val, 1]
+
             collect_values(node.left, level+1) if node.left else None
             collect_values(node.right, level+1) if node.right else None
             return
-        collect_values(root, level) if root else  None
+        collect_values(root, 0) if root else  None
 
-        for idx, value in enumerate(sums):
-            sums[idx] = (value/counts[idx])
+        i=0
+        result = []
+        while counts:
+            result.append(counts[i][0]/counts[i][1])
+            del counts[i]
+            i+=1
+            
 
-        return sums
+        return result
 
 l2 = TreeNode(15)
 r2 = TreeNode(7)
