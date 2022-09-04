4ebcd39cb651df6505ab09ffc4e641945d1962c8
Suleiman Hesham <45403646+Suleiman99Hesham@users.noreply.github.com>
2022-09-04 12:08:32 +0200

    Time: 337 ms (6.24%), Space: 18.1 MB (85.09%) - LeetHub

--- 56-merge-intervals/56-merge-intervals.py
+++ 56-merge-intervals/56-merge-intervals.py
@@ -3,8 +3,6 @@ class Solution:
         i = 0
         intervals = sorted(intervals, key=lambda x: x[0])
         while i < len(intervals)-1:
-            print(intervals[i+1][0], intervals[i][0], intervals[i+1][1], intervals[i][1])
-            print(max(intervals[i+1][0], intervals[i][0]) , min(intervals[i+1][1], intervals[i][1]))
             if max(intervals[i+1][0], intervals[i][0]) <= min(intervals[i+1][1], intervals[i][1]):
                 intervals[i][0] = min(intervals[i+1][0], intervals[i][0])
                 intervals[i][1] = max(intervals[i+1][1], intervals[i][1])
@@ -12,8 +10,3 @@ class Solution:
                 continue
             i += 1
         return intervals
-
-
-print(Solution().merge([[1,3],[2,6],[8,10],[15,18]]))
-print(Solution().merge([[1,4],[0,4]]))
-print(Solution().merge([[2,3],[4,5],[6,7],[8,9],[1,10]]))
