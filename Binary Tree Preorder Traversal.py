93d2674ef407622ff6e4cb75019756565d7b660e
Soliman Hesham <soliman.hesham@uniparticle.com>
2023-01-10 12:44:25 +0200

    Binary Tree Preorder Traversal

--- Binary Tree Preorder Traversal.py
+++ Binary Tree Preorder Traversal.py
@@ -6,30 +6,19 @@ class TreeNode:
         self.right = right
 class Solution:
     def preorderTraversal(self, root: TreeNode ) -> list:
-        if not root:
-            return []
-        answer=[]
+        if not root: return []
+        array = []
         stack = []
         while True:
-            answer.append(root.val)
-            if type(root.left) == TreeNode:
-                if root.right:
-                    stack.append(root.right)
-                root=root.left
+            array.append(root.val)
+            if root.right:
+                stack.append(root.right)
+            if root.left:
+                root = root.left
+            elif stack:
+                root = stack.pop()
             else:
-                if type(root.left) == int:
-                    answer.append(root.left)
-                if type(root.right) == TreeNode:
-                    root=root.right
-                else:
-                    if type(root.right) == int:
-                        answer.append(root.right)
-                    if len(stack) > 0:
-                        root = stack.pop()
-                    else: stack = None
-            if stack == None:
-                break
-        return answer
+                return array
 n5=None
 n4=TreeNode(3)
 n3=TreeNode(2,n4,n5)
@@ -38,3 +27,33 @@ root=TreeNode(1,n2,n3)
 
 obj=Solution()
 print(obj.preorderTraversal(root))
+
+
+
+
+# class Solution:
+#     def preorderTraversal(self, root: TreeNode ) -> list:
+#         if not root:
+#             return []
+#         answer=[]
+#         stack = []
+#         while True:
+#             answer.append(root.val)
+#             if type(root.left) == TreeNode:
+#                 if root.right:
+#                     stack.append(root.right)
+#                 root=root.left
+#             else:
+#                 if type(root.left) == int:
+#                     answer.append(root.left)
+#                 if type(root.right) == TreeNode:
+#                     root=root.right
+#                 else:
+#                     if type(root.right) == int:
+#                         answer.append(root.right)
+#                     if len(stack) > 0:
+#                         root = stack.pop()
+#                     else: stack = None
+#             if stack == None:
+#                 break
+#         return answer
